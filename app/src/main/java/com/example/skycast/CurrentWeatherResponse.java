e410cc26c03225ede3c0e44cab6fc7614448b3d9
Alex Argueta <alexarguetanet@gmail.com>
2022-05-18 00:17:49 -0400

    Current weather is displayed on the home screen

---
+++ app/src/main/java/com/example/skycast/CurrentWeatherResponse.java
@@ -0,0 +1,474 @@
+package com.example.skycast;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class CurrentWeatherResponse implements Serializable {
+
+    Coord coord;
+    ArrayList<Weather> weather;
+    String base;
+    Main main;
+    double visibility;
+    Wind wind;
+    Clouds clouds;
+    int dt;
+    Sys sys;
+    int timezone;
+    int id;
+    String name;
+    int cod;
+
+    public CurrentWeatherResponse() {
+    }
+
+    public CurrentWeatherResponse(Coord coord, ArrayList<Weather> weather, String base, Main main, double visibility, Wind wind, Clouds clouds, int dt, Sys sys, int timezone, int id, String name, int cod) {
+        this.coord = coord;
+        this.weather = weather;
+        this.base = base;
+        this.main = main;
+        this.visibility = visibility;
+        this.wind = wind;
+        this.clouds = clouds;
+        this.dt = dt;
+        this.sys = sys;
+        this.timezone = timezone;
+        this.id = id;
+        this.name = name;
+        this.cod = cod;
+    }
+
+
+    public Coord getCoord() {
+        return coord;
+    }
+
+    public void setCoord(Coord coord) {
+        this.coord = coord;
+    }
+
+    public ArrayList<Weather> getWeather() {
+        return weather;
+    }
+
+    public void setWeather(ArrayList<Weather> weather) {
+        this.weather = weather;
+    }
+
+    public String getBase() {
+        return base;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public Main getMain() {
+        return main;
+    }
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+
+    public double getVisibility() {
+        return visibility;
+    }
+
+    public void setVisibility(double visibility) {
+        this.visibility = visibility;
+    }
+
+    public Wind getWind() {
+        return wind;
+    }
+
+    public void setWind(Wind wind) {
+        this.wind = wind;
+    }
+
+    public Clouds getClouds() {
+        return clouds;
+    }
+
+    public void setClouds(Clouds clouds) {
+        this.clouds = clouds;
+    }
+
+    public int getDt() {
+        return dt;
+    }
+
+    public void setDt(int dt) {
+        this.dt = dt;
+    }
+
+    public Sys getSys() {
+        return sys;
+    }
+
+    public void setSys(Sys sys) {
+        this.sys = sys;
+    }
+
+    public int getTimezone() {
+        return timezone;
+    }
+
+    public void setTimezone(int timezone) {
+        this.timezone = timezone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCod() {
+        return cod;
+    }
+
+    public void setCod(int cod) {
+        this.cod = cod;
+    }
+
+    @Override
+    public String toString() {
+        return "CurrentWeatherResponse{" +
+                "coord=" + coord +
+                ", weather=" + weather +
+                ", base='" + base + '\'' +
+                ", main=" + main +
+                ", visibility=" + visibility +
+                ", wind=" + wind +
+                ", clouds=" + clouds +
+                ", dt=" + dt +
+                ", sys=" + sys +
+                ", timezone=" + timezone +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                ", cod=" + cod +
+                '}';
+    }
+}
+
+class Weather implements Serializable {
+
+    int id;
+    String main;
+    String description;
+    String icon;
+
+    public Weather() {
+    }
+
+    public Weather(int id, String main, String description, String icon) {
+        this.id = id;
+        this.main = main;
+        this.description = description;
+        this.icon = icon;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMain() {
+        return main;
+    }
+
+    public void setMain(String main) {
+        this.main = main;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    @Override
+    public String toString() {
+        return "Weather{" +
+                "id=" + id +
+                ", main='" + main + '\'' +
+                ", description='" + description + '\'' +
+                ", icon='" + icon + '\'' +
+                '}';
+    }
+}
+
+
+class Coord implements Serializable {
+
+    double lon;
+    double lat;
+
+    public Coord() {
+    }
+
+    public Coord(double lon, double lat) {
+        this.lon = lon;
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    @Override
+    public String toString() {
+        return "Coord{" +
+                "lon=" + lon +
+                ", lat=" + lat +
+                '}';
+    }
+}
+
+class Main implements Serializable {
+
+    double temp;
+    double feels_like;
+    double temp_min;
+    double temp_max;
+    double pressure;
+    double humidity;
+
+    public Main() {
+    }
+
+    public Main(double temp, double feels_like, double temp_min, double temp_max, double pressure, double humidity) {
+        this.temp = temp;
+        this.feels_like = feels_like;
+        this.temp_min = temp_min;
+        this.temp_max = temp_max;
+        this.pressure = pressure;
+        this.humidity = humidity;
+    }
+
+    public double getTemp() {
+        return temp;
+    }
+
+    public void setTemp(double temp) {
+        this.temp = temp;
+    }
+
+    public double getFeels_like() {
+        return feels_like;
+    }
+
+    public void setFeels_like(double feels_like) {
+        this.feels_like = feels_like;
+    }
+
+    public double getTemp_min() {
+        return temp_min;
+    }
+
+    public void setTemp_min(double temp_min) {
+        this.temp_min = temp_min;
+    }
+
+    public double getTemp_max() {
+        return temp_max;
+    }
+
+    public void setTemp_max(double temp_max) {
+        this.temp_max = temp_max;
+    }
+
+    public double getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(double pressure) {
+        this.pressure = pressure;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+
+    @Override
+    public String toString() {
+        return "Main{" +
+                "temp=" + temp +
+                ", feels_like=" + feels_like +
+                ", temp_min=" + temp_min +
+                ", temp_max=" + temp_max +
+                ", pressure=" + pressure +
+                ", humidity=" + humidity +
+                '}';
+    }
+}
+
+class Wind implements Serializable {
+
+    double speed;
+    double deg;
+
+    public Wind() {
+    }
+
+    public Wind(double speed, double deg) {
+        this.speed = speed;
+        this.deg = deg;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getDeg() {
+        return deg;
+    }
+
+    public void setDeg(double deg) {
+        this.deg = deg;
+    }
+}
+
+class Clouds implements Serializable {
+
+    double all;
+
+    public Clouds() {
+    }
+
+    public Clouds(double all) {
+        this.all = all;
+    }
+
+    public double getAll() {
+        return all;
+    }
+
+    public void setAll(double all) {
+        this.all = all;
+    }
+
+    @Override
+    public String toString() {
+        return "Clouds{" +
+                "all=" + all +
+                '}';
+    }
+}
+
+class Sys implements Serializable {
+
+    int type;
+    int id;
+    String country;
+    int sunrise;
+    int sunset;
+
+    public Sys() {
+    }
+
+    public Sys(int type, int id, String country, int sunrise, int sunset) {
+        this.type = type;
+        this.id = id;
+        this.country = country;
+        this.sunrise = sunrise;
+        this.sunset = sunset;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getSunrise() {
+        return sunrise;
+    }
+
+    public void setSunrise(int sunrise) {
+        this.sunrise = sunrise;
+    }
+
+    public int getSunset() {
+        return sunset;
+    }
+
+    public void setSunset(int sunset) {
+        this.sunset = sunset;
+    }
+
+    @Override
+    public String toString() {
+        return "Sys{" +
+                "type=" + type +
+                ", id=" + id +
+                ", country='" + country + '\'' +
+                ", sunrise=" + sunrise +
+                ", sunset=" + sunset +
+                '}';
+    }
+}
--- app/src/main/java/com/example/skycast/HomeFragment.java
+++ app/src/main/java/com/example/skycast/HomeFragment.java
@@ -1,18 +1,34 @@
 package com.example.skycast;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttp;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,10 +37,18 @@ import okhttp3.Request;
  */
 public class HomeFragment extends Fragment {
 
+    // API key: 833110e347976d143c39463d4742e9fc
+    // Default zip code: 28223
+    // URL:  https://api.openweathermap.org/data/2.5/weather?zip=28223,us&appid=833110e347976d143c39463d4742e9fc&units=imperial
+
     IHome mListener;
     final String TAG = "demoss";
     OkHttpClient client = new OkHttpClient();
-
+    Handler mHandler;
+    ImageView currWeatherImage;
+    TextView currTempText;
+    TextView weatherDescText;
+    TextView cityNameText;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -73,9 +97,45 @@ public class HomeFragment extends Fragment {
 
         getActivity().setTitle("Skycast");
 
+        // Initializing class fields
+        currWeatherImage = view.findViewById(R.id.currWeatherImage);
+        currTempText = view.findViewById(R.id.currTempText);
+        weatherDescText = view.findViewById(R.id.weatherDescText);
+        cityNameText = view.findViewById(R.id.cityNameText);
+
+
+        // Method to get the HTTP response containing the weather data
+        getCurrentWeather();
 
 
 
+
+        // Thread handler to change UI components according to HTTPS Response
+        mHandler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(@NonNull Message msg) {
+
+                // Create alert dialog if the device has no internet connection
+                if (msg.getData().getInt("API_RESPONSE_STATUS") == 1) {
+                    String failedMsgString = "Please make sure you are connected to wifi or cellular data";
+                    createAlert("No Internet Connection", failedMsgString);
+
+                    return false;
+                }
+
+                CurrentWeatherResponse currWeather = (CurrentWeatherResponse) msg.getData().getSerializable("CURRENT_WEATHER");
+
+                // Generate weather icon. Use https in URL
+                Picasso.get().load("https://openweathermap.org/img/wn/" + currWeather.weather.get(0).icon + "@4x.png").into(currWeatherImage);
+
+                currTempText.setText(String.valueOf(currWeather.main.temp + "\u00B0"));
+                cityNameText.setText(currWeather.name);
+                weatherDescText.setText(currWeather.weather.get(0).description);
+
+
+                return false;
+            }
+        });
 
 
         // Inflate the layout for this fragment
@@ -83,11 +143,74 @@ public class HomeFragment extends Fragment {
     }
 
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        /*
+            Will make the API call each time the user goes back to the app
+            when it is running in the background.
+         */
+        getCurrentWeather();
+    }
+
     public void getCurrentWeather() {
 
+        // Create request object
+        Request request = new Request.Builder()
+                .url("https://api.openweathermap.org/data/2.5/weather?zip=28223,us&appid=833110e347976d143c39463d4742e9fc&units=imperial")
+                .build();
+
+        // Initiate API call
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                // API call has failed
+                Message failMsg = new Message();
+                Bundle bundle = new Bundle();
+
+                bundle.putInt("API_RESPONSE_STATUS", 1);
+
+                failMsg.setData(bundle);
+                mHandler.sendMessage(failMsg);
+
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                // API call is successful
+                // Parse JSON data
+                Gson gson = new Gson();
+                CurrentWeatherResponse currWeather = gson.fromJson(response.body().charStream(), CurrentWeatherResponse.class);
+
+                // Create message to send to the main thread
+                Message weatherMsg = new Message();
+                Bundle bundle = new Bundle();
+
+                bundle.putSerializable("CURRENT_WEATHER", currWeather);
+                bundle.putInt("API_RESPONSE_STATUS", 0);
+
+                weatherMsg.setData(bundle);
+                mHandler.sendMessage(weatherMsg);
+
+            }
+        });
 
 
+    }
 
+
+    public void createAlert(String title, String message) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+        alert.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("Dismiss", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .create()
+                .show();
     }
 
 
